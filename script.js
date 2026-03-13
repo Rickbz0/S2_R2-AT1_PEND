79ab3c74b1005a1e89ba454e3cf5331fbfda7337
HENRIQUE FELIPE BEZERRA OLIVEIRA <henrique.oliveira116@senaisp.edu.br>
2026-03-13 12:44:55 -0300

    atividade 2 finalizada

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
     let texto = document.getElementById("texto");
-    let btnMudar = document.getElementById("btnMudar");
+    let btnMudar = document.querySelector("#btnMudar");
 
     btnMudar.addEventListener("click", () => {
         texto.innerText = "Texto alterado com sucesso pelo JS";
