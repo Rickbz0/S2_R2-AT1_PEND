e32d763d4cfb42a41f49af5dcfca4653c4651560
HENRIQUE FELIPE BEZERRA OLIVEIRA <henrique.oliveira116@senaisp.edu.br>
2026-03-13 13:42:36 -0300

    atividade 4 finalizada

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-let input = document.getElementById("inputTarefa");
-let botao = document.querySelector("#btnAdicionar");
+let tarefa = document.getElementById("tarefa");
+let btn = document.querySelector("#btn");
 let mensagem = document.getElementById("mensagem");
-let lista = document.getElementById("listaTarefas");
+let lista = document.getElementById("lista");
 
-botao.addEventListener("click", () => {
+btn.addEventListener("click", () => {
 
-    let texto = input.value.trim();
+    let texto = tarefa.value;
 
     if (texto === "") {
 
         mensagem.textContent = "preencha a tarefa";
         mensagem.className = "text-danger fw-bold";
 
-        return;
+    } else {
+
+        let li = document.createElement("li");
+
+        li.textContent = texto;
+
+        li.classList.add("list-group-item");
+
+        lista.appendChild(li);
+
+        tarefa.value = "";
+
+        mensagem.textContent = "a tarefa adicionada com sucesso";
+        mensagem.className = "text-success fw-bold";
     }
 
-    let li = document.createElement("li");
-
-    li.textContent = texto;
-
-    li.classList.add("list-group-item");
-
-    lista.appendChild(li);
-
-    input.value = "";
-
-    mensagem.textContent = "a tarefa adicionada com sucesso";
-    mensagem.className = "text-success fw-bold";
 });
