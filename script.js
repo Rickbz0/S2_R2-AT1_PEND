69e98797b3cb23d7db11ac589ada6546585715f2
HENRIQUE FELIPE BEZERRA OLIVEIRA <henrique.oliveira116@senaisp.edu.br>
2026-03-13 12:43:22 -0300

    atividade 4

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let nomeAqui = document.getElementById("nomeAqui");
-let btnEntrar = document.querySelector("#btnEntrar");
-let mensagem = document.getElementById("mensagem");
+let input = document.getElementById("itemInput");
+let botao = document.getElementById("btnAdicionar");
+let lista = document.getElementById("listaTarefas");
 
-    btnEntrar.addEventListener("click", () => {
-    mensagem.innerText = `Olá ${nomeAqui.value}`;
-    
+botao.addEventListener("click", () => {
+
+    let texto = input.value;
+
+    if(texto === "") return;
+
+    let li = document.createElement("li");
+
+    li.textContent = texto;
+
+    li.classList.add("list-group-item");
+
+    lista.appendChild(li);
+
+    input.value = "";
 });
