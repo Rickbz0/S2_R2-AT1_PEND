b0124121812e1eb6a831d45a3520465892446335
HENRIQUE FELIPE BEZERRA OLIVEIRA <henrique.oliveira116@senaisp.edu.br>
2026-03-13 12:36:38 -0300

    atividade 3 finalizada

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-    let texto = document.getElementById("texto");
-    let btnMudar = document.getElementById("btnMudar");
+let input = document.getElementById("itemInput");
+let botao = document.getElementById("btnAdicionar");
+let lista = document.getElementById("lista");
 
-    btnMudar.addEventListener("click", () => {
-        texto.innerText = "Texto alterado com sucesso pelo JS";
-    });
+botao.addEventListener("click", () => {
+
+    let texto = input.value;
+
+    if(texto === "") return;
+
+    let li = document.createElement("li");
+
+    li.textContent = texto;
+
+    li.classList.add("list-group-item");
+
+    lista.appendChild(li);
+
+    input.value = "";
+});
