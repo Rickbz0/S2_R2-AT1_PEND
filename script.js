74580f5326e92269ccc38fae1730a3dae990b6ac
HENRIQUE FELIPE BEZERRA OLIVEIRA <henrique.oliveira116@senaisp.edu.br>
2026-03-20 10:43:54 -0300

    atv6 finalizada

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let tarefa = document.getElementById("tarefa");
+let tarefa = document.querySelector("#tarefa");
 let btn = document.querySelector("#btn");
 let mensagem = document.getElementById("mensagem");
 let lista = document.getElementById("lista");
@@ -10,34 +10,50 @@ btn.addEventListener("click", () => {
     if (texto === "") {
 
         mensagem.textContent = "preencha a tarefa";
-        mensagem.className = "text-danger fw-bold";
+        mensagem.className = "text-danger fw-bold text-center";
 
     } else {
 
         let li = document.createElement("li");
+        li.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
 
-        li.classList.add("list-group-item");
+        // isso vai servir para deixar a tarefa marcada (checkbox - marcar caixa)
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("form-check-input", "me-2");
 
-        li.textContent = texto;
+        let textoNode = document.createTextNode(texto);
 
         let btnApagar = document.createElement("button");
-
         btnApagar.textContent = "Remover";
+        btnApagar.classList.add("btn", "btn-danger", "btn-sm");
 
-        btnApagar.classList.add("btn","btn-danger","ms-3");
+        checkbox.addEventListener("change", () => {
+// quando marcar a tarefa, ficara vermelha, assim que desmarcar, voltara a ser preta
+            if (checkbox.checked) {
+                li.style.textDecoration = "line-through";
+                li.style.color = "red";
+            } else {
+                li.style.textDecoration = "none";
+                li.style.color = "black";
+            }
+
+        });
 
         btnApagar.addEventListener("click", () => {
             li.remove();
         });
 
+        li.appendChild(checkbox);
+        li.appendChild(textoNode);
         li.appendChild(btnApagar);
 
         lista.appendChild(li);
 
         tarefa.value = "";
 
-        mensagem.textContent = "a tarefa adicionada com sucesso";
-        mensagem.className = "text-success fw-bold";
+        mensagem.textContent = "tarefa adicionada com sucesso";
+        mensagem.className = "text-success fw-bold text-center";
     }
 
 });
